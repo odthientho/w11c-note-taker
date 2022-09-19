58f36f6b9c7d4bd8c46869059b4023b80610c150
Arthur <odthientho@gmail.com>
2022-09-19 12:32:00 -0400

    connecting to server: index.html nodes.html

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require('path');
+
+const PORT = 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+})
+
+app.get('/api/notes', (req, res) => {
+});
+
+// POST request to add a review
+app.post('/api/notes', (req, res) => {
+});
+
+app.listen(PORT, () =>
+  console.log("Server Port: " + PORT)
+);
